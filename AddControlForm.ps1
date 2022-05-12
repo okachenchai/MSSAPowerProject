c59e8c29e2d40a6a586eac10be6961c27af5b338
okachenchai <105228676+okachenchai@users.noreply.github.com>
2022-05-12 08:38:33 -0500

    Create AddControlForm.ps1

---
+++ AddControlForm.ps1
@@ -0,0 +1,6 @@
+# Add the controls to the Form
+$form.controls.add($button)
+$form.controls.add($ActionBox)
+$form.controls.add($mainlabel)
+$form.controls.add($label)
+$form.controls.add($global:textfield)
