6a6a90605273aecbffbd24b12a3b33243e3c4334
okachenchai <105228676+okachenchai@users.noreply.github.com>
2022-05-12 08:29:31 -0500

    Create CreateMainLabel.ps1

---
+++ CreateMainLabel.ps1
@@ -0,0 +1,5 @@
+# Create Main Form Label 
+$mainlabel = New-Object Windows.Forms.Label
+$mainlabel.Location = New-Object Drawing.Point 130,10
+$mainlabel.Size = New-Object Drawing.Point 400,15
+$mainlabel.text = "Use this program to manipulate user accounts in Active Directory"
