1add0dc1a561406c426efd9cbf1f0e5a5a3c0336
okachenchai <105228676+okachenchai@users.noreply.github.com>
2022-05-12 08:31:47 -0500

    Create CreateTxtbxSetSzeLocation.ps1

---
+++ CreateTxtbxSetSzeLocation.ps1
@@ -0,0 +1,4 @@
+# Create TextBox and set text, size and location
+$global:textfield = New-Object Windows.Forms.TextBox
+$global:textfield.Location = New-Object Drawing.Point 275,75
+$global:textfield.Size = New-Object Drawing.Point 200,30
